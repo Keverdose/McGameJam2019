0c6a4d10fb4b45e627dcb3908dd9f8eaada35734
Kevin Ye <yekevin1995@hotmail.com>
2019-03-15 20:06:50 -0400

    Working move script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, moveY);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
