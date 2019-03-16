51a542af4c7f027158c95241511f5b2d2d78fb27
kalecj <kalecj@hotmail.com>
2019-03-15 22:45:51 -0400

    made the ufo beam up cow still needs sounds, needs maybe animation for now to save cow hold space bar on 15 second timer

--- Assets/Scripts/EnemyUFOScript.cs
+++ Assets/Scripts/EnemyUFOScript.cs
@@ -11,10 +11,15 @@ public class EnemyUFOScript : MonoBehaviour
     public Vector3 originalTargetLocation;
     public float ufoSpeed = 0.75f;
     public float ufoHeight = 0.4f;
+    public Vector3 ufoOriginalLocation;
+    public float ufoTime = 15.0f;
+    public float ufoTimer;
     // Start is called before the first frame update
     void Start()
     {
         hunting = true;
+        ufoTimer = 0.0f;
+        ufoOriginalLocation = transform.position;
         originalTargetLocation = target.transform.position;
         targetLocation = target.transform.position;
         targetLocation.y += ufoHeight;
@@ -23,21 +28,36 @@ public class EnemyUFOScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ufoTimer += Time.deltaTime;
+        if(ufoTimer >= ufoTime)
+        {
+            hunting = true;
+        }
         if(hunting)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetLocation, ufoSpeed * Time.deltaTime);
             if(transform.position == targetLocation)
             {
-                if(Input.GetKeyDown("space"))
+                if (Input.GetKey("space"))
                 {
-                    print("GOOD STUFF");
+                    target.transform.position = Vector2.MoveTowards(target.transform.position, originalTargetLocation, 0.1f * Time.deltaTime);
                 }
-                else
+                else 
                 {
                     target.transform.position = Vector2.MoveTowards(target.transform.position, transform.position, 0.1f * Time.deltaTime);
                 }
-                
+                if (target.transform.position == originalTargetLocation)
+                {
+                    ufoTimer = 0.0f;
+                    hunting = false;
+                }
             }
+        }
+        else
+        {
+            print("DONE");
+            
+            transform.position = Vector2.MoveTowards(transform.position, ufoOriginalLocation, ufoSpeed * Time.deltaTime);
         }
     }
 }
